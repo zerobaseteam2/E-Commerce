df0d38629510e468b2e807533670a8f0c228a9bd
sebastian <scbaecp@naver.com>
2023-12-05 11:43:04 +0900

    chore: 코드 작성 표준 정리

--- src/main/java/com/example/Ecommerce/user/controller/UserController.java
+++ src/main/java/com/example/Ecommerce/user/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.example.Ecommerce.user.controller;
 
+import com.example.Ecommerce.user.dto.UserRegisterDto;
+import com.example.Ecommerce.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
+
+  private final UserService userService;
+
+  public ResponseEntity<UserRegisterDto.Response> registerUser(
+      @RequestBody UserRegisterDto.Request request) {
+    UserRegisterDto.Response response = userService.register(request);
+    return ResponseEntity.ok(response);
+  }
 }
