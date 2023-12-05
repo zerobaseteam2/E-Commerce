c75ecaaaa221932a4f34b15af3e455b134a48130
Xylitol311 <cktnghd0311@daum.net>
2023-12-05 10:48:03 +0900

    feat : 초기 패키지 및 클래스 설정

---
+++ src/main/java/com/example/Ecommerce/exception/CustomException.java
@@ -0,0 +1,14 @@
+package com.example.Ecommerce.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    private ErrorCode errorCode;
+    @Override
+    public String toString() {
+        return errorCode + " : " + errorCode.getMessage();
+    }
+}
---
+++ src/main/java/com/example/Ecommerce/member/service/impl/MemberServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.Ecommerce.member.service.impl;
+
+public class MemberServiceImpl {
+}
