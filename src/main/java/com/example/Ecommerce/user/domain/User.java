ae9783278fb7b9ad5aa40304ed3ecda9f8ecb865
Xylitol311 <cktnghd0311@daum.net>
2023-12-07 17:33:48 +0900

    feat : User Entity 구현
    - User Entity와 UserRole Enum 구현.
    - UserRole
    	- CUSTOMER
    	- SELLER
    	- ADMIN
    
    Issues: #12

--- src/main/java/com/example/Ecommerce/user/domain/User.java
+++ src/main/java/com/example/Ecommerce/user/domain/User.java
@@ -1,22 +1,39 @@
 package com.example.Ecommerce.user.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class User {
-
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  private String userId;
+  private String password;
+  private String name;
+  private String email;
+  private String phone;
+  private Date birth;
+  @Enumerated(value = EnumType.STRING)
+  private UserRole role;
+  @CreatedDate
+  private Date createdAt;
+  @LastModifiedDate
+  private Date updateAt;
 }
---
+++ src/main/java/com/example/Ecommerce/user/domain/UserRole.java
@@ -0,0 +1,7 @@
+package com.example.Ecommerce.user.domain;
+
+public enum UserRole {
+  CUSTOMER,
+  SELLER,
+  ADMIN
+}
