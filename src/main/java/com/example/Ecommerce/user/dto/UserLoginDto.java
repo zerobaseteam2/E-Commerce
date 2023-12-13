d91a4ad51dd08c932a2e5228871b71b65a899465
sebastian <scbaecp@naver.com>
2023-12-13 11:16:14 +0900

    chore : 인증 발송 이메일 설정 수정2

--- src/main/java/com/example/Ecommerce/user/dto/UserLoginDto.java
+++ src/main/java/com/example/Ecommerce/user/dto/UserLoginDto.java
@@ -1,25 +1,28 @@
 package com.example.Ecommerce.user.dto;
 
+import static com.example.Ecommerce.security.jwt.JwtTokenUtil.BEARER_PREFIX;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import static com.example.Ecommerce.security.jwt.JwtTokenUtil.BEARER_PREFIX;
 
 public class UserLoginDto {
   @Getter
   @Setter
   @Builder
+  @NoArgsConstructor
   @AllArgsConstructor
   public static class Request {
-    private String username;
+    private String userId;
     private String password;
   }
   
   @Getter
   @Setter
   @Builder
+  @NoArgsConstructor
   @AllArgsConstructor
   public static class Response {
     private String accessToken;
