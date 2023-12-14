b2ea36b9ddccc7b42c6d6080131e717142e0080d
sebastian <scbaecp@naver.com>
2023-12-14 22:26:59 +0900

    feat : 배송지 추가 API
    Issues: #21

--- src/main/java/com/example/Ecommerce/user/service/UserService.java
+++ src/main/java/com/example/Ecommerce/user/service/UserService.java
@@ -1,17 +1,21 @@
 package com.example.Ecommerce.user.service;
 
+import com.example.Ecommerce.security.UserDetailsImpl;
+import com.example.Ecommerce.user.dto.UserAddressDto;
 import com.example.Ecommerce.user.dto.UserLoginDto;
 import com.example.Ecommerce.user.dto.UserRegisterDto;
 
 public interface UserService {
-  
-  public UserRegisterDto.Response registerUser(UserRegisterDto.Request request);
-  
-  public UserLoginDto.Response login(UserLoginDto.Request request);
-  
-  public UserLoginDto.Response reissue(String refreshToken);
-  
-  public void logout(String accessToken, String username);
-  
+
+  UserRegisterDto.Response registerUser(UserRegisterDto.Request request);
+
+  UserLoginDto.Response login(UserLoginDto.Request request);
+
+  UserLoginDto.Response reissue(String refreshToken);
+
+  void logout(String accessToken, String username);
+
   void verifyUserEmail(Long id);
+
+  void addUserAddress(UserAddressDto.Request request, UserDetailsImpl userDetails);
 }
