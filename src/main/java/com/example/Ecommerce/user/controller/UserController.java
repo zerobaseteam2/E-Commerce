232daec0381da175068dceba8de5a845ff7c3b66
sebastian <scbaecp@naver.com>
2023-12-14 22:28:42 +0900

    Revert "feat : 배송지 추가 API"
    This reverts commit b2ea36b9ddccc7b42c6d6080131e717142e0080d.

--- src/main/java/com/example/Ecommerce/user/controller/UserController.java
+++ src/main/java/com/example/Ecommerce/user/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.example.Ecommerce.user.controller;
 
 import com.example.Ecommerce.security.UserDetailsImpl;
-import com.example.Ecommerce.user.dto.UserAddressDto;
 import com.example.Ecommerce.user.dto.UserLoginDto;
 import com.example.Ecommerce.user.dto.UserRegisterDto;
 import com.example.Ecommerce.user.service.UserService;
@@ -9,60 +8,43 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
-
+  
   private final UserService userService;
-
+  
   @PostMapping("/register")
   public ResponseEntity<UserRegisterDto.Response> registerUser(
-      @RequestBody @Valid UserRegisterDto.Request request) {
+          @RequestBody @Valid UserRegisterDto.Request request) {
     UserRegisterDto.Response response = userService.registerUser(request);
-
+    
     return ResponseEntity.ok(response);
   }
-
+  
   @GetMapping("/verify/{id}")
-  public ResponseEntity<Void> verifyUserEmail(@PathVariable Long id) {
+  public ResponseEntity verifyUserEmail(@PathVariable Long id) {
     userService.verifyUserEmail(id);
-
+    
     return ResponseEntity.ok().build();
   }
-
+  
   @PostMapping("/login")
   public ResponseEntity<UserLoginDto.Response> login(@RequestBody UserLoginDto.Request request) {
     return ResponseEntity.ok(userService.login(request));
   }
-
+  
   @PostMapping("/reissue")
-  public ResponseEntity<UserLoginDto.Response> reissue(
-      @RequestHeader("RefreshToken") String refreshToken) {
+  public ResponseEntity<UserLoginDto.Response> reissue(@RequestHeader("RefreshToken") String refreshToken) {
     return ResponseEntity.ok(userService.reissue(refreshToken));
   }
-
+  
   @PostMapping("/logout")
-  public void logout(@RequestHeader("Authorization") String accessToken,
-      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+  public void logout(@RequestHeader("Authorization") String accessToken, @AuthenticationPrincipal UserDetailsImpl userDetails) {
     String username = userDetails.getUser().getUserId();
     userService.logout(accessToken, username);
-  }
-
-  @PostMapping("/address")
-  public ResponseEntity<Void> addUserAddress(
-      @RequestBody @Valid UserAddressDto.Request request,
-      @AuthenticationPrincipal UserDetailsImpl userDetails) {
-    userService.addUserAddress(request, userDetails);
-
-    return ResponseEntity.ok().build();
   }
 }
--- src/main/java/com/example/Ecommerce/user/service/UserService.java
+++ src/main/java/com/example/Ecommerce/user/service/UserService.java
@@ -1,21 +1,17 @@
 package com.example.Ecommerce.user.service;
 
-import com.example.Ecommerce.security.UserDetailsImpl;
-import com.example.Ecommerce.user.dto.UserAddressDto;
 import com.example.Ecommerce.user.dto.UserLoginDto;
 import com.example.Ecommerce.user.dto.UserRegisterDto;
 
 public interface UserService {
-
-  UserRegisterDto.Response registerUser(UserRegisterDto.Request request);
-
-  UserLoginDto.Response login(UserLoginDto.Request request);
-
-  UserLoginDto.Response reissue(String refreshToken);
-
-  void logout(String accessToken, String username);
-
+  
+  public UserRegisterDto.Response registerUser(UserRegisterDto.Request request);
+  
+  public UserLoginDto.Response login(UserLoginDto.Request request);
+  
+  public UserLoginDto.Response reissue(String refreshToken);
+  
+  public void logout(String accessToken, String username);
+  
   void verifyUserEmail(Long id);
-
-  void addUserAddress(UserAddressDto.Request request, UserDetailsImpl userDetails);
 }
