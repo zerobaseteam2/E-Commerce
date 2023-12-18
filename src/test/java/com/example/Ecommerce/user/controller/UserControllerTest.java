7489f64d92e39d234ac8ead4db8323ff789be3a7
Xylitol311 <cktnghd0311@daum.net>
2023-12-19 07:48:22 +0900

    test : login 테스트 구현

--- src/test/java/com/example/Ecommerce/user/controller/UserControllerTest.java
+++ src/test/java/com/example/Ecommerce/user/controller/UserControllerTest.java
@@ -1,6 +1,31 @@
 package com.example.Ecommerce.user.controller;
 
+import com.example.Ecommerce.config.TestConfig;
+import com.example.Ecommerce.user.domain.User;
+import com.example.Ecommerce.user.dto.UserAddressDto;
+import com.example.Ecommerce.user.dto.UserLoginDto;
+import com.example.Ecommerce.user.dto.UserRegisterDto;
+import com.example.Ecommerce.user.repository.UserRepository;
+import com.example.Ecommerce.user.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.Date;
+
+import static com.example.Ecommerce.security.jwt.JwtTokenUtil.AUTHORIZATION_HEADER;
 import static com.example.Ecommerce.user.domain.UserRole.CUSTOMER;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
@@ -11,111 +36,142 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.example.Ecommerce.config.TestConfig;
-import com.example.Ecommerce.user.dto.UserAddressDto;
-import com.example.Ecommerce.user.dto.UserRegisterDto;
-import com.example.Ecommerce.user.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Date;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
-
 @Import(TestConfig.class)
 @WebMvcTest(UserController.class)
 class UserControllerTest {
-
+  
   @Autowired
   private MockMvc mockMvc;
-
+  
   @Autowired
   private ObjectMapper objectMapper;
-
+  
   @MockBean
   private UserService userService;
-
+  
+  @MockBean
+  private UserRepository userRepository;
+  
+  @MockBean
+  private PasswordEncoder passwordEncoder;
+  
+  
   @Test
   @DisplayName("회원 가입 성공 테스트")
   void registerUserSuccess() throws Exception {
     //given
     given(userService.registerUser(any()))
-        .willReturn(UserRegisterDto.Response.builder()
-            .userId("Test")
-            .build());
-
+            .willReturn(UserRegisterDto.Response.builder()
+                    .userId("Test")
+                    .build());
+    
     UserRegisterDto.Request request = UserRegisterDto.Request.builder()
-        .userId("Test")
-        .password("Test1234!")
-        .name("테스트")
-        .email("Test@naver.com")
-        .phone("01012345678")
-        .birth(new Date())
-        .role(CUSTOMER)
-        .build();
+            .userId("Test")
+            .password("Test1234!")
+            .name("테스트")
+            .email("Test@naver.com")
+            .phone("01012345678")
+            .birth(new Date())
+            .role(CUSTOMER)
+            .build();
     //when
     //then
     mockMvc.perform(
-            post("/api/user/register")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.userId").exists())
-        .andDo(print());
-
+                    post("/api/user/register")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.userId").exists())
+            .andDo(print());
+    
     verify(userService).registerUser(any());
   }
-
+  
+  @Test
+  @DisplayName("로그인 성공 테스트")
+  void loginUserSuccess() throws Exception {
+    //given
+    User user = User.builder()
+            .userId("Test")
+            .password(passwordEncoder.encode("Test1234!"))
+            .name("테스트")
+            .email("Test@naver.com")
+            .phone("01012345678")
+            .birth(new Date())
+            .role(CUSTOMER)
+            .build();
+    userRepository.save(user);
+    
+    UserLoginDto.Request request = UserLoginDto.Request.builder()
+            .userId("Test")
+            .password("Test1234!")
+            .build();
+    
+    
+    given(userService.login(any(UserLoginDto.Request.class)))
+            .willReturn(UserLoginDto.Response.of(
+                    "Bearer access_token_value",
+                    "Bearer refresh_token_value"));
+    
+    //when
+    
+    //then
+    MvcResult result = mockMvc.perform(post("/api/user/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isOk())
+            .andReturn();
+    String token = result.getResponse().getHeader(AUTHORIZATION_HEADER);
+    
+    assertNotNull(token);
+    assertTrue(token.startsWith("Bearer "));
+  }
+  
   @Test
   @DisplayName("배송지 추가 성공 테스트")
   @WithMockUser(authorities = {"ROLE_CUSTOMER"})
   void registerUserAddressSuccess() throws Exception {
     //given
     UserAddressDto.Request request = UserAddressDto.Request.builder()
-        .roadAddress("서울특별시 샘플구 테스트로 1")
-        .detailAddress("101동 101호")
-        .zoneNo("12345")
-        .addressName("집")
-        .phone("01012345678")
-        .build();
+            .roadAddress("서울특별시 샘플구 테스트로 1")
+            .detailAddress("101동 101호")
+            .zoneNo("12345")
+            .addressName("집")
+            .phone("01012345678")
+            .build();
     //when
     //then
     mockMvc.perform(
-            post("/api/user/address")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
-        .andExpect(status().isOk())
-        .andDo(print());
-
+                    post("/api/user/address")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isOk())
+            .andDo(print());
+    
     verify(userService).addUserAddress(any(), any());
   }
-
+  
   @Test
   @DisplayName("배송지 수정 성공 테스트")
   @WithMockUser(authorities = {"ROLE_CUSTOMER"})
   void modifyUserAddressSuccess() throws Exception {
     //given
     UserAddressDto.Request request = UserAddressDto.Request.builder()
-        .roadAddress("서울특별시 샘플구 테스트로 1")
-        .detailAddress("101동 101호")
-        .zoneNo("12345")
-        .addressName("집")
-        .phone("01012345678")
-        .build();
+            .roadAddress("서울특별시 샘플구 테스트로 1")
+            .detailAddress("101동 101호")
+            .zoneNo("12345")
+            .addressName("집")
+            .phone("01012345678")
+            .build();
     //when
     //then
     mockMvc.perform(
-            put("/api/user/address/1")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
-        .andExpect(status().isOk())
-        .andDo(print());
-
+                    put("/api/user/address/1")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isOk())
+            .andDo(print());
+    
     verify(userService).modifyUserAddress(any(), any(), anyLong());
   }
 }
